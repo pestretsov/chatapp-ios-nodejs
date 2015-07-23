cf557b697350a6c01845834b54a4b1016a7908db
Artemy Pestretsov <pestretsov@me.com>
2015-07-24 02:00:31 +0300

    Initial Commit

---
+++ ChatApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatApp
+//
+//  Created by Artemy Pestretsov on 7/24/15.
+//  Copyright (c) 2015 Artemy Pestretsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChatApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatApp
+//
+//  Created by Artemy Pestretsov on 7/24/15.
+//  Copyright (c) 2015 Artemy Pestretsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
